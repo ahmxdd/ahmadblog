42294a34b7408969f91c45077b6279bece5c794f
Ahmad Ali <121513836+ahmxdd@users.noreply.github.com>
2024-12-28 15:01:11 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Ahmad Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Ahmad Ali`,
+      summary: `goes to places.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
